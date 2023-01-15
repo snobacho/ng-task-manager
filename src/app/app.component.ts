b71e49540f5becdc9b589fd4884bd17cd6728495
snobacho <giorgicheli@gmail.com>
2023-01-15 22:22:32 +0400

    add randomCOlor function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,11 @@ import { Todo } from './Todos';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    todos: Todo[]= []
-    newTodo: string
+  todos: Todo[]= []
+  newTodo: string  
    
 
-    addTodo(){
-    
+  addTodo(){
     if(this.newTodo){
       let todo = new Todo
       todo.name = this.newTodo
@@ -23,9 +22,7 @@ export class AppComponent {
     } else {
       alert('please add task')
     }
- 
   }
-
 
   done(id: number) {
     this.todos[id].isCompleted = !this.todos[id].isCompleted
@@ -34,6 +31,16 @@ export class AppComponent {
   removeTodo(id: number){
     console.log();
       this.todos = this.todos.filter((y, i) => i !==id)
+  }
+
+// * random color *******
+
+  getRandomColor() {
+    let length = 6;
+    let chars = '0123456789ABCDEF';
+    let hex = '#';
+    while(length--) hex += chars[(Math.random() * 16) | 0];
+    return hex;
   }
 
 }
